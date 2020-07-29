a3dd4e8b1637ad3d301d335630637efac25bede2
Franziska Löw <franzi@localyzeapp.com>
2020-07-29 10:34:44 +0200

    Deploy storybook ghpages (#1668)
    * Adds script to export storybook to docs folder
    
    * Runs npm run build-storybook
    
    Co-authored-by: franziloew <franziska@localyze.de>

---
+++ precache-manifest.7a359d7f189874cbe005244fd5572b74.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46107847cff81297c1f7894025d33ff4",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a1b58fd69341d76c05ac.bundle.js"
+  },
+  {
+    "url": "runtime~main.a1b58fd69341d76c05ac.bundle.js"
+  },
+  {
+    "revision": "66906cbb3aa99875d721",
+    "url": "static/css/main.fd878e9c.chunk.css"
+  },
+  {
+    "revision": "056a373d808ada4f12a2d6538e88823c",
+    "url": "static/media/lato-v16-latin-regular.056a373d.eot"
+  },
+  {
+    "revision": "9087e4a6aceecc9b2914823044951a3a",
+    "url": "static/media/lato-v16-latin-regular.9087e4a6.svg"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "static/media/lato-v16-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "static/media/lato-v16-latin-regular.b8ee546a.woff"
+  },
+  {
+    "revision": "d1d5e30aa8bb081cad8f409f27861672",
+    "url": "static/media/lato-v16-latin-regular.d1d5e30a.ttf"
+  },
+  {
+    "revision": "44e6f9d47c5265e079617dea31d2deb0",
+    "url": "static/media/logo-localize.44e6f9d4.png"
+  },
+  {
+    "revision": "049a929c5d81988b3ae6d2f985ca7aa5",
+    "url": "static/media/open-sans-v17-latin-regular.049a929c.ttf"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "static/media/open-sans-v17-latin-regular.33543c5c.woff2"
+  },
+  {
+    "revision": "5f4d4bc11d64b6cb605b7030c1997270",
+    "url": "static/media/open-sans-v17-latin-regular.5f4d4bc1.eot"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "static/media/open-sans-v17-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "static/media/open-sans-v17-latin-regular.de0869e3.woff"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "4be1a572fca40bcb2202504cb17aed91",
+    "url": "static/media/roboto-v20-latin-regular.4be1a572.eot"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "static/media/roboto-v20-latin-regular.8681f434.svg"
+  },
+  {
+    "revision": "3a84dabf4be40dd8af437970553728b5",
+    "url": "static/media/yantramanav-v5-latin-regular.3a84dabf.ttf"
+  },
+  {
+    "revision": "5cfab3562be67831142feed2fa57f349",
+    "url": "static/media/yantramanav-v5-latin-regular.5cfab356.eot"
+  },
+  {
+    "revision": "744da80528c90d0555d19bdac3894f80",
+    "url": "static/media/yantramanav-v5-latin-regular.744da805.woff"
+  },
+  {
+    "revision": "bf85b6cdc9f1352bbb4ff1e8bc3367f1",
+    "url": "static/media/yantramanav-v5-latin-regular.bf85b6cd.svg"
+  },
+  {
+    "revision": "c31cb91e5fb813604ad89ab0c623808e",
+    "url": "static/media/yantramanav-v5-latin-regular.c31cb91e.woff2"
+  },
+  {
+    "url": "vendors~main.a1b58fd69341d76c05ac.bundle.js"
+  },
+  {
+    "url": "vendors~main.a1b58fd69341d76c05ac.bundle.js.LICENSE.txt"
+  }
+]);
