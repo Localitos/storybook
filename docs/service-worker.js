720a5aa0f21eca04545e398156c96b51357efc0e
franziloew <manoj.pesit@gmail.com>
2020-09-09 09:39:29 +0000

    Deploying to master from  @ 6545acba54d2d58a79ee6b929209013fb04c0745 🚀

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.94a75dfe19539ccc117010da89fcb7a8.js"
+  "precache-manifest.28e04bce81c459e53700fc4cbdd43b93.js"
 );
 
 self.addEventListener('message', (event) => {
