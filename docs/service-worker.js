78811bafe798d3a5653cf9442598819894bb1f70
franziloew <ignacioruizcabrol@gmail.com>
2020-10-13 09:16:00 +0000

    Deploying to master from  @ 6ae6e550eb16c07acd09374f62bac81a6ad316ce 🚀

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c0ea290c1d07f058756de85c0077b9e0.js"
+  "precache-manifest.9b5b66e8369ab0ca3cf92b6f2ef78906.js"
 );
 
 self.addEventListener('message', (event) => {
